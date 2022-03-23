5112560259d8143f742a4c859edb67a0c9d98ca3
Asmahan <asmahanhmohamed@gmail.com>
2022-03-23 12:10:34 +0000

    Syntax update.

--- robot-simulator.js
+++ robot-simulator.js
@@ -1,6 +1,8 @@
 // [X Y BEARING COMMANDS]
 
-/*module.exports =*/ class Robot {
+// const robotDirections = ['NORTH', 'EAST', 'SOUTH', 'WEST']
+
+class Robot {
 execute(string) {
     let stringArr = string.split(" "); //Each 'segment' of the string instruction will be split up and turned into an array.
     this.x = parseInt(stringArr[0]); //The index specifies the order/sequencing of the instructions.
@@ -8,8 +10,13 @@ execute(string) {
     this.bearing = stringArr[2].toUpperCase();
     this.commands = stringArr[3].toUpperCase();
 
+    // if (!robotDirections.includes(direction)) {
+    //     throw 'Invalid Bearing'
+    //   }
+
         for (const direction of this.commands) {
             switch(direction) {
+
                 case 'R': //If the direction is right we move clockwise...
                 if (this.bearing == 'NORTH') {this.bearing == 'EAST'} //turning right when facing north means the robot will now face east
                 if (this.bearing == 'EAST') {this.bearing == 'SOUTH'} //turning right when facing east means the robot will now face south
@@ -27,18 +34,17 @@ execute(string) {
                 break;
 
                 case 'A': //North and South are on the y axis. East and West are on the x axis.
-                if (this.bearing == 'NORTH') {this.y+1} //move up one position on the y axis
-                if (this.bearing == 'EAST') {this.x+1} //move up one position on the x axis
-                if (this.bearing == 'SOUTH') {this.y-1} //move down one position on the y axis
-                if (this.bearing == 'WEST') {this.x-1} //move left one position on the x axis
+                if (this.bearing == 'NORTH') {this.y++} //move up one position on the y axis
+                if (this.bearing == 'EAST') {this.x++} //move up one position on the x axis
+                if (this.bearing == 'SOUTH') {this.y--} //move down one position on the y axis
+                if (this.bearing == 'WEST') {this.x--} //move left one position on the x axis
                 
                 // break;
             }
         }
-    return `${this.x} ${this.y} ${this.bearing}`
+    return `${this.x} ${this.y} ${this.bearing}`;
 }
 }
 
 module.exports = Robot 
 
-// console.log((robot.execute("0 0 NORTH AAAALAAAALAAAALAAAAL")));
